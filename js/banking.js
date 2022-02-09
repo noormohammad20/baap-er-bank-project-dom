20efc3c6a18fefd673a0b1750ad5cb5facf275c0
noormohammad20 <mdsaim204@gmail.com>
2022-02-09 22:55:03 +0600

    Get User deposit and set deposit total value

---
+++ js/banking.js
@@ -0,0 +1,13 @@
+// Handle Deposit Button Event
+
+document.getElementById('deposit-button').addEventListener('click', function () {
+    // get the amount deposited
+    const depositInput = document.getElementById('deposit-input')
+    const depositAmount = depositInput.value
+
+    const depositTotal = document.getElementById('deposit-total')
+    depositTotal.innerText = depositAmount;
+
+    // clear the deposit input field
+    depositInput.value = '';
+})
