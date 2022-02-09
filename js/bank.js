da3210f9f8b2b04ee7a9283183c7898c8725c253
noormohammad20 <mdsaim204@gmail.com>
2022-02-09 21:35:30 +0600

    Deposit, withdraw, balance area using grid layout

--- js/bank.js
+++ js/bank.js
@@ -5,7 +5,7 @@ document.getElementById('login-submit').addEventListener('click', function () {
     // get user password
     const passwordField = document.getElementById('user-password')
     const userPassword = passwordField.value
-
+    // check email and password 
     if (userEmail == 'sontan@gmail.com' && userPassword == 'secret') {
         window.location.href = 'banking.html'
     }
